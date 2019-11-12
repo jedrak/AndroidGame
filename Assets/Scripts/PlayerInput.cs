62f32a008a9ea5019e1ad16f04ef6e5eb69cd6fb
Andrzej Zabski <jedrak32@gmail.com>
2019-11-12 10:45:04 +0100

    initial commit

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(Input.acceleration);
+        Debug.DrawLine(transform.position, Input.acceleration, Color.cyan);
+        rb.AddForce(Quaternion.Euler(90, 0, 0) * Input.acceleration);
+    }
+}
